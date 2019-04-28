03fa3c8ec2d724ac739c39f217590bfb4d59474c
James Pitt <c3333018@uon.edu.au>
2019-04-28 23:47:45 +0000

    Progress

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
 #include <stdio.h> 
 
-int main() {}
+int main() {
+    
+    int letters;
+    printf("Amount of letters?"");
+    scanf("%d", &letters);
+    char string[letters];
+    printf("What is you word?");
+    scanf("%s", string);
+} 
