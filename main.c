058030dbac5e2b1b0993bfbc6c6032b45f98d3b5
James Pitt <c3333018@uon.edu.au>
2019-04-28 23:57:30 +0000

    Progress

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 int main() {
     
     int letters;
-    printf("Amount of letters?"");
+    printf("Amount of letters?");
     scanf("%d", &letters);
     char string[letters];
     printf("What is you word?");
-    scanf("%s", string);
+    scanf("%s", string); 
+     
+    int ChooseOperation;
+    printf("(1) Encryption\n"); 
+    printf("(2) Decryption\n");
+    scanf("%d", &ChooseOperation);
+    
 } 
